92da2bedba3b762940721ca7cdc1b32d0dbc655c
Marisol Poma <marisol.poma15@hotmail.com>
2019-06-11 22:35:27 -0300

    Aula 11_06_2019

---
+++ app/src/main/java/br/com/etechoracio/etec_solicitao_ferias/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.etec_solicitao_ferias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
